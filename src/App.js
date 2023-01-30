54bd714ae1616c3f75c0e52e76a35116a21f45b3
ihor-vip <vip20140902@gmail.com>
2023-01-30 16:22:50 +0200

    todo list, data saved in local storage, module css system, react icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import {Header} from "./components/Header";
+import {Tasks} from "./components/Tasks";
+
+const LOCAL_STORAGE_KEY = 'todo:tasks';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState([]);
+
+    function loadSavedTasks() {
+        const saved = localStorage.getItem(LOCAL_STORAGE_KEY);
+        if (saved) {
+            setTasks(JSON.parse(saved));
+        }
+    }
+
+    function setTasksAndSave(newTasks) {
+        setTasks(newTasks);
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTasks));
+    }
+
+    useEffect(() => {
+        loadSavedTasks();
+    }, [])
+
+    function addTask(taskTitle) {
+        setTasksAndSave([...tasks, {
+            id: crypto.randomUUID(),
+            title: taskTitle,
+            isCompleted: false
+        }]);
+    }
+
+    function deleteTaskById(taskId) {
+        const newTasks = tasks.filter(task => task.id !== taskId);
+        setTasksAndSave(newTasks);
+    }
+
+    function toggleTaskCompletedById(taskId) {
+        const newTasks = tasks.map(task => {
+            if (task.id === taskId) {
+                return {
+                    ...task,
+                    isCompleted: !task.isCompleted
+                }
+            }
+            return task;
+        });
+        setTasksAndSave(newTasks);
+    }
+
+    return (
+        <>
+            <Header handleAddTask={addTask}/>
+            <Tasks
+                tasks={tasks}
+                onDelete={deleteTaskById}
+                onComplete={toggleTaskCompletedById}
+            />
+        </>
+    )
 }
 
-export default App;
+export default App
